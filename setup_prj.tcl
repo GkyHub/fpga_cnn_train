6924109465a5cddf26c9f38b4264d043d30b4bfa
gky <gkycl@163.com>
2018-01-09 16:09:00 +0800

    add project tcl script

---
+++ setup_prj.tcl
@@ -0,0 +1,15 @@
+# project property
+# FPGA chip
+set     CHIP    xcku115-flvb2104-2-e
+# development board
+set     BOARD   xilinx.com:kcu1500:part0:1.1
+
+# create project
+create_project project_1 ./vivado_prj -part $CHIP
+set_property board_part $BOARD [current_project]
+
+# add source files
+add_files -fileset sources_1 ./src
+update_compile_order -fileset sources_1
+add_files -fileset sim_1 ./sim
+update_compile_order -fileset sim_1
